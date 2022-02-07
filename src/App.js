309018ffdb7d54f049427f6549e0dc067156d123
Parikshit Hooda <phooda804@live.com>
2022-02-07 22:30:09 +0530

    setup <header> in App.js

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ const SEARCH_API = `https://api.themoviedb.org/3/search/movie?&api_key=${APIKEY}
 
 function App() {
   const [movies, setMovies] = useState([]);
+  const [searchTerm, setSearchTerm] = useState("");
 
   const getMovies = (API) => {
     fetch(API)
@@ -21,6 +22,17 @@ function App() {
 
   return (
     <>
+      <header>
+        <form>
+          <input
+            type="search"
+            className="search"
+            placeholder="Search..."
+            value={searchTerm}
+          />
+        </form>
+      </header>
+
       <div className="movie-container">
         {movies.length > 0 &&
           movies.map((movie) => <Movie key={movie.id} {...movie} />)}
