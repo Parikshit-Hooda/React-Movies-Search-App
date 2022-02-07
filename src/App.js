4ed6cae62b1e82da0cc50584f9906fbccf6a074e
Parikshit Hooda <phooda804@live.com>
2022-02-07 22:47:10 +0530

    setup search functionality

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
 import React, { useState, useEffect } from "react";
 import Movie from "./components/Movie";
-import "./styles.css";
 import { APIKEY } from "../apiKey.js";
 
 const FEATURED_API = `https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=${APIKEY}`;
-const SEARCH_API = `https://api.themoviedb.org/3/search/movie?&api_key=${APIKEY}=`;
+const SEARCH_API = `https://api.themoviedb.org/3/search/movie?api_key=${APIKEY}&language=en-US&page=1&include_adult=false&query=`;
 
 function App() {
   const [movies, setMovies] = useState([]);
